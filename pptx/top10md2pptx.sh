24b5a9949bcd06226579fb6fa0be411079d3d5f5
SPoint42 <seb@gioria.org>
2025-03-24 15:00:51 +0100

    Update OWASP Top 10 for LLM 2025 articles with improved formatting and clarity

---
+++ pptx/top10md2pptx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-02/2025-02-20-OWASPTop10LLM2025.md  ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-02/2025-02-26-prompt.md  ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-02/2025-02-27-prompt2.md  ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-02/2025-02-28-prompt3.md  ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-01-prompt4.md  ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-03-LLM02-2025.md  ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-04-LLM02-2025-Protection.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-04-LLM02-2025-Protection.md ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-05-LLM02-2025-Protection-suite.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-05-LLM02-2025-Protection-suite.md ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-06-LLM03-2025.md ./template.pptx                 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-07-LLM04-2025.md ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-09-LLM05-2025.md ./template.pptx
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-10-LLM06-2025.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-11-LLM07-2025.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-13-LLM08-2025.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-14-LLM09-2025.md ./template.pptx 
+sh ../scripts/md2ppt-pandoc.sh ../_posts/fr/2025-03/2025-03-16-LLM10-2025.md ./template.pptx
+
