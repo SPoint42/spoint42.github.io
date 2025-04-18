9fca1ac138193ed64ff272beec8a0a4300faf725
SPoint42 <seb@gioria.org>
2025-04-18 14:43:14 +0200

    Refactor RSS feed parser to enhance error handling and improve file organization

--- scripts/veille/pocGemini.py
+++ scripts/veille/pocGemini.py
@@ -1,10 +1,11 @@
-import google.generativeai as genai
 import os
 import xml.etree.ElementTree as ET
 
 from dotenv import load_dotenv
 import requests
 from datetime import date
+from google import genai
+DEBUG = False
 
 def parse_rss(xml_file):
     """Parse le fichier RSS et extrait les contenus clés"""
@@ -20,78 +21,116 @@ def parse_rss(xml_file):
     return "\n".join(articles)[:5000]  # Limite pour respecter le contexte
 
 
-# Charger les variables d'environnement depuis un fichier .env
+
 load_dotenv()
 
-# Configurer l'API avec votre clé
 api_key = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=api_key)
-# Sélectionner le modèle Gemini à utiliser
-model = genai.GenerativeModel('gemini-2.0-flash')
-
+client = genai.Client(api_key=api_key)
 
 rss_urls = (
     ["https://feeds.feedburner.com/TheHackersNews", "The Hacker News"],
-    ["https://www.cert.ssi.gouv.fr/feed/", "CERT-FR"]
+    ["https://www.cert.ssi.gouv.fr/feed/", "CERT-FR"],
+    ["https://aws.amazon.com/security/security-bulletins/rss/feed/", "AWS Security Bulletins"],
+    ["https://feeds.feedburner.com/GoogleOnlineSecurityBlog", "Google Online Security Blog"]
 )
 
-
+today = date.today().strftime("%Y-%m-%d")
+os.mkdir(f"{today}") if not os.path.exists(f"{today}") else None
+tabFile = list()
 
 for url in rss_urls:
-    print(f"Récupération du flux RSS depuis {url[0]}...")
-    response = requests.get(url[0])
-    if response.status_code != 200:
-        raise Exception(f"Erreur lors de la récupération du flux RSS : {response.status_code}")
-    print (f"Flux RSS récupéré avec succès depuis {url[0]}")
-    with open(f"{url[1]}temp.rss", 'w') as f:
-        f.write(response.text)
-    rss_content = parse_rss(f"{url[1]}temp.rss")
+    try :
+        print(f"Récupération du flux RSS depuis {url[0]}...")
+        response = requests.get(url[0])
+        if response.status_code != 200:
+            print (f"Erreur lors de la récupération du flux RSS : {response.status_code}")
+        else :
+            print (f"Flux RSS récupéré avec succès depuis {url[0]}")
 
-    format_article  = f"""    
-    Titre de l'article 
-        Résumé de l'article (#résumé-de-larticle)
-        Score CVSS + Score EPSS + Lien vers l'article + Lien vers la page CVE
-    """
+            with open(f"{today}/{url[1]}temp.rss", 'w') as f:
+                f.write(response.text)
+            rss_content = parse_rss(f"{today}/{url[1]}temp.rss")
+            tabFile.append (client.files.upload(file=f"{today}/{url[1]}temp.rss"))
+    except :
+        print (f"Erreur lors de la récupération du flux RSS depuis {url[0]} : {response.status_code}")
+        continue
 
 
-    prompt = f"""
-Agis comme un expert en veille informatique. Voici le contenu d'un  flux RSS :
-{rss_content}
-Tu dois analyser les articles parlant de vulnérabilités uniquement.
+format_article  = f"""
+---
+layout: post
+title: "Veille automatisée du {today}"
+date: {today}
+categories:
+  - veille
+  - vulnérabilités
+  - sécurité
+---
 
-Tu dois respecter les consignes suivantes :
-2. Ne pas inclure d'articles qui ne parlent pas de cybersécurité
-3. Ne pas inclure d'articles qui ne parlent pas de menaces
+Voici la veille de sécurité du {today} :
 
-Pour chacun des articles, génère un element structuré comportant :
+# ⚠️Alertes de sécurité importantes (CVSS > 8.5)⚠️
+    Place le titre des articles  dont la CVSS est supérieure a 8.5 
+
+# Sommaire
+Place ici le sommaire
+   
+## Place ici le ritre de l'article 
+    Place ici le résumé de l'article (#résumé-de-larticle)
+* CVE si existante + lien vers la page de la CVE 
+* Score CVSS (si existant sinon afficher inconnu) + Score EPSS ((si existant sinon afficher inconnu) 
+
+"""
+
+prompt = f"""
+Tu es un expert en cybersécurité et tu dois analyser ces {tabFile} représentant le contenu de ta veille du {today} 
+et provenant de test flux RSS. 
+
+
+
+Tu dois respecter les consignes suivantes : 
+- Tu dois analyser ces fichiers pour créer un seul document que tu publiera 
+sur ton blog de veille a destination de l'ensemble de tes équipes. Le document que tu publiera doit uniquement comporter des 
+éléments  publiés ou mis a jour dans ces fichiers rss le {today}.
+- Tu dois analyser les articles parlant de vulnérabilités ou d'attaque zero-day uniquement.
+- Je veux que tu ne prennes en compte que les articles dont la date de publication est le {today}.
+- Ne pas inclure d'articles qui ne parlent pas de cybersécurité
+- Ne pas inclure d'articles qui ne parlent pas de menaces
+- Tu dois effectuer ton analyse uniquement  sur les elements fournis dans les fichiers {tabFile} et ne pas inclure 
+d'autres sources.
+
+Lorsque tu trouves un contenu qui parle de vulnérabilités ou d'attaques zero-day, tu dois créer un element  structuré (
+article) comportant :
     - le résumé de l'article
     - le lien hypertexte vers l'article 
     - l'identifiant CVE(si existant sinon mettre Inconnue + le score CVSS(si existant sinon mettre Inconnu) + le score 
-    EPSS(si existant sinon mettre Inconnu ) le lien hypertexte vers la page CVE concernée
+    EPSS(si existant sinon mettre Inconnu ) ainsi le lien hypertexte vers la page CVE concernée
+    - la source de donnees de l'article (ex: The Hacker News, CERT-FR, ...)
+    
+Lorsque tous les {tabFile} sont traités, tu dois créer un sommaire comportant les titres des articles et les liens vers les articles
+Le sommaire doit être généré en fonction des titres des articles et comporter les liens vers les articles.
+Le contenu doit être traduit en francais. 
+Tu peux  inclure des emoticons pour rendre le contenu plus attrayant
 
 Je ne souhaite pas avoir d'explication de tes choix, je n'ai pas besoin que le fichier indique le format yaml ou 
-markdown, ni que tu me donnes autre chose que le contenu brut pour que je 
-puisse directement le publier avec  jekyll.
+markdown, ni que tu me donnes autre chose que le contenu brut pour que je  puisse directement le publier avec  jekyll.
 
-Le sommaire doit être généré en fonction des titres des articles et comporter les liens vers les articles.
-Le contenu doit être en français. 
-Tu dois inclure des emoticons pour rendre le contenu plus attrayant
-
-Le format de ta réponse doit etre le  suivant 
 
 {format_article}
 """
 
-    today = date.today().strftime("%Y-%m-%d")
-    response = model.generate_content(prompt)
-    markdown_filename = f"{today}-veille-{url[1]}.md"
-    with open(markdown_filename, "a", encoding="utf-8") as md_file:
-        # Écris le contenu de la réponse
-        md_file.write(response.text)
+if DEBUG :
+    print("Prompt :\n")
+    print(prompt)
+    print("\n\n")
+    print("Liste des modèles disponibles :\n")
+    for m in client.models.list():
+        print(f"""{m.display_name} : {m.name}""")
 
-## ensuite on creer un seul fichier depuis tous les fichiers
-## parcourir tous les fichiers de veille et en creer un seul, puis
-prompt = f"""
-Voici le contenu plusieurs  fichiers de  veille {veille}. Je souhaite que tu me les fusionnes en un seul fichier markdown.
-"""
+response = client.models.generate_content(model='gemma-3-27b-it',
+contents= prompt)
+
+markdown_filename = f"{today}-veille.md"
+with open(markdown_filename, "w", encoding="utf-8") as md_file:
+    md_file.write(response.text)
 
