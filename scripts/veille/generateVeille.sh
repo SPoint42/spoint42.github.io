6cad93b452dd78157176c2ce61ee717655c65207
SPoint42 <seb@gioria.org>
2025-04-29 13:08:28 +0200

    Add GitHub Actions workflow for generating Veille reports

--- scripts/veille/generateVeille.sh
+++ scripts/veille/generateVeille.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 pocGemini.py
-python3 pocGemini.py -m gemini-2.5-flash-preview-04-17
-python3 pocGemini.py -m gemini-2.5-pro-exp-03-25
+python3 scripts/veille/pocGemini.py
+python3 scripts/veille/pocGemini.py -m gemini-2.5-flash-preview-04-17
+python3 scripts/veille/pocGemini.py -m gemini-2.5-pro-exp-03-25
